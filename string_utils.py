314fc1df6e1bdc2392f29fbe0ab962a1fd526b94
netaashkenazi <neta.ashkenazi.email@gmail.com>
2025-11-17 21:08:36 +0200

    Update string_utils.py 1

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,32 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
-
+    start = 0 
+    end = 0
+    i = 0
+    split_formula = []
+    if not formula:
+        return []
+    for i in range(1,len(formula)):
+      end = i
+      if formula[i].isupper() : 
+       split_formula.append(formula[start:end])
+       start = end
+    split_formula.append(formula[start:])
+    return split_formula
+    
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+   digit_location = 1
+   new_formula = ()
+   for char in formula[1:]:
+      if char.isdigit():
+          break
+      digit_location += 1
+
+   if digit_location == len(formula):
+       return formula,1
+   else :
+      figure = int(formula[digit_location:])
+      return formula[:digit_location],figure
+
+   
+       
